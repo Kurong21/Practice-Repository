e05e694f8df5ce4203881af73d8c9a0c592f1029
tengrui <1470909186@qq.com>
2024-01-26 17:29:43 +0800

    原生js实现购物车逻辑

---
+++ shoppingCart/data.js
@@ -0,0 +1,42 @@
+var goods = [
+    {
+        pic: './assets/1.png',
+        title: '椰云拿铁',
+        desc: '原创椰云toppping',
+        favorRate: 95,
+        price: 19.9,
+        instock: 100
+    },
+    {
+        pic: './assets/1.png',
+        title: '生椰拿铁',
+        desc: '优质冷榨椰浆',
+        favorRate: 95,
+        price: 32,
+        instock: 190
+    },
+    {
+        pic: './assets/1.png',
+        title: '加浓美式',
+        desc: '清醒加倍',
+        favorRate: 90,
+        price: 48,
+        instock: 11
+    },
+    {
+        pic: './assets/1.png',
+        title: '瓦尔登蓝钻瑞纳冰',
+        desc: '德玛西亚',
+        favorRate: 65,
+        price: 12,
+        instock: 5
+    },
+    {
+        pic: './assets/1.png',
+        title: '椰云精粹美式',
+        desc: '不用吸管，大口吞食',
+        favorRate: 85,
+        price: 20,
+        instock: 89
+    }
+];
